e98edc953ce4819ab5b977711733e5ff65560758
Mitul Pravinbhai Malani <mt215690@dal.ca>
2022-04-02 18:22:56 -0300

    feat: implemented cart feature

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -2,13 +2,21 @@
  * @author Mitul Pravinbhai Malani (B00869519)
  * This component works as a wrapper for Books page
  */
+import { useContext, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 
 import BooksHeader from "../../components/BooksHeader";
 import BooksList from "../../components/BooksList";
 import BooksFilter from "../../components/BooksFilter";
+import { BooksContext } from "../../Providers/BooksProvider";
 
 const Books = () => {
+  const { getUserCart } = useContext(BooksContext);
+
+  useEffect(() => {
+    getUserCart();
+  }, []);
+
   return (
     <Container className="books-container">
       <BooksHeader />
