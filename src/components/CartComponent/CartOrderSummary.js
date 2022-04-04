e58b779cc775865fbcfe92272692624ef0d96c73
PRIT THAKKAR <pr360648@dal.ca>
2022-04-04 09:12:24 -0300

    Added checkout functionality

--- src/components/CartComponent/CartOrderSummary.js
+++ src/components/CartComponent/CartOrderSummary.js
@@ -2,13 +2,13 @@
  * @author Mitul Pravinbhai Malani (B00869519)
  * Cart order summary component, which shows order summary with checkout option
  */
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import Form from "react-bootstrap/Form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 import { BooksContext } from "../../Providers/BooksProvider";
-
+import  PaymentModel  from "../Payments/PaymentModal"
 const CartOrderSummary = () => {
   const { cart } = useContext(BooksContext);
 
@@ -18,7 +18,14 @@ const CartOrderSummary = () => {
     return totalPrice.toFixed(2);
   };
 
+  const [openModal, setOpenModal] = useState(false);
+  const toggleModal = () => {
+    setOpenModal((prev) => !prev);
+  };
+
   return (
+    <>
+    <PaymentModel show={openModal} handleClose={toggleModal} totalPrice = {getTotalPrice()}></PaymentModel>
     <div className="cart-order-summary">
       <div className="order-summary-title">Order Summary</div>
       <div className="order-summary-wrapper">
@@ -34,7 +41,7 @@ const CartOrderSummary = () => {
             <div>Total Price</div>
             <div>{`$${getTotalPrice()}`}</div>
           </div>
-          <button className="checkout-button">
+          <button className="checkout-button" onClick={toggleModal}>
             <FontAwesomeIcon
               icon={faCartShopping}
               color="#FFF"
@@ -45,6 +52,7 @@ const CartOrderSummary = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
